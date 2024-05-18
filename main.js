c599515bdc6399e37cb6b682f86e783e57a1865f
Disguied-Coffee <danielidanque@gmail.com>
2024-05-18 16:13:58 -0500

    X and Y binded controls and tunnels!

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 import './style.css'
 
 import { SCREENSIZES } from './constants'; // Pulling some data out of a constants folder 
-import Phaser from 'phaser';
+import Phaser, { Core } from 'phaser';
 import js from 'easystarjs'
 
 let input = document.querySelector("input");
@@ -14,6 +14,7 @@ let playerSpeed = 230;
 
 class GameScene extends Phaser.Scene {
 
+  teleporterTimer;
   constructor() {
     super("scene-game");
     this.player; //adds the variable player to the game obj
@@ -23,6 +24,9 @@ class GameScene extends Phaser.Scene {
     this.dir = 0;
 
     this.isPaused = false;
+
+    this.playerCanTeleport = false;
+    
 
     // this.finder = new js();
 
@@ -73,13 +77,15 @@ class GameScene extends Phaser.Scene {
   preload() {
     //grab the background image from server
     this.load.image("tiles", "../assets/terrain.png");
-    this.load.tilemapTiledJSON("map", "../assets/map2.json");
+    this.load.tilemapTiledJSON("map", "../tiledAssets/mapTest3/map.json");
 
     this.load.multiatlas('player', '../assets/atlas/blah.json', '../assets/atlas');
   }
 
   // present assets on screen, load assets in client
   create() {
+
+    //funny finder lib c:
     this.finder = js.js();
     
     this.map = this.make.tilemap({ key: "map" });
@@ -87,29 +93,30 @@ class GameScene extends Phaser.Scene {
     const tileset = this.map.addTilesetImage("mapy", "tiles"); //add the titleset to the 
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const belowLayer = this.map.createLayer("floor", tileset, 0, 0);
-    const tunnels = this.map.createLayer("tunnels", tileset, 0, 0);
-    const worldLayer = this.map.createLayer("structures", tileset, 0, 0);
-    const islandLayer = this.map.createLayer("islands", tileset, 0, 0);
+    this.layer = this.map.createLayer("floor", tileset, 0, 0);
 
     this.player = this.physics.add.sprite(100, 100, 'player', 'blah.png');
 
     this.player.setScale(0.75);
 
-    worldLayer.setCollisionByProperty({ collides: true });
-    islandLayer.setCollisionByProperty({ collides: true });
-
     this.player.setCollideWorldBounds(true);
 
-    this.map.filterTiles(tiles => console.log(tiles))
 
-    this.physics.add.collider(this.player, worldLayer);
+    this.leftRightTP = this.map.filterTiles(tile => tile.index === 6);
+    this.upDownTP = this.map.filterTiles(tile => tile.index === 5);
 
-    this.physics.add.collider(this.player, islandLayer);
+    this.map.setCollision([3,8,7,11,9]);
 
 
     //tunnels
+    this.leftSpawn = this.map.findObject("Objects", obj => obj.name === "SpawnPointRight");
+    this.rightSpawn = this.map.findObject("Objects", obj => obj.name === "SpawnPointLeft");
+    this.upSpawn = this.map.findObject("Objects", obj => obj.name === "SpawnPointUp");
+    this.bottomSpawn = this.map.findObject("Objects", obj => obj.name === "SpawnPointBottom");
 
+    //tunnel timer
+    this.teleporterTimer = this.time.addEvent({delay: 2000});
+    this.time.addEvent(this.teleporterTimer);
 
     //obj to hold 
     this.cursor = this.input.keyboard.createCursorKeys();
@@ -118,9 +125,49 @@ class GameScene extends Phaser.Scene {
     this.setRandomDir();
   }
 
+
+
+  checkForTeleportX(player, tile){
+    //if the player has teelported already, don't let them tp again until they're fully off
+    console.log(this.teleporterTimer.getProgress());
+    if(this.teleporterTimer.getProgress() == 1){
+      this.time.addEvent(this.teleporterTimer);
+      if(player.x < 200){
+        player.setX(this.leftSpawn.x);
+      }
+      else if(player.x > 200){
+        player.setX(this.rightSpawn.x)
+      }
+
+    }
+    
+  }
+
+  checkForTeleportY(player, tile){
+    //if the player has teelported already, don't let them tp again until they're fully off
+    console.log(this.teleporterTimer.getProgress());
+    if(this.teleporterTimer.getProgress() == 1){
+      this.time.addEvent(this.teleporterTimer);
+      if(player.y > 200){
+        player.setY(this.upSpawn.y);
+      }
+      else if(player.y < 200){
+        player.setY(this.bottomSpawn.y);
+      }
+
+    }
+    
+  }
+
   // update values
   update() {
 
+    // check for teleport
+    this.physics.world.overlapTiles(this.player,this.leftRightTP,this.checkForTeleportX,null,this);
+    this.physics.world.overlapTiles(this.player,this.upDownTP,this.checkForTeleportY,null,this);
+
+
+    this.physics.collide(this.player, this.layer)
     //PAUSING... CUT BY MVP
     // if(catchPause == true) this.isPaused = !this.isPaused;
 
@@ -137,58 +184,101 @@ class GameScene extends Phaser.Scene {
     // this.player.y.
 
     // for directions
+    
+
+    this.handleMovement();
+  }
+  
+  teleportPlayer(){
+    console.log("SUCCESS!");
+  }
+  
+  /**
+   * 
+   * @param {boolean} setXorY - true --> X | false --> Y
+   * 
+   * X is for when you go L or R from straight
+   * Y is for when you go up from L or R
+   * 
+   * meant to fix bug with staying at a single position for too long.
+   */
+  setPlayerToNearestCord(setXorY){
+    //BREAKDOWN:
+    // get player's coords, find the nearest tile with coords, and with 
+    // that info find the XY of that tile.
+
+    // map.getTileAt --> get tile
+
+    // tile.x --> get X coord.
+    
+    // console.log(this.player.x);
+    // console.log(this.player.y);
+
+    const coords = this.player.getCenter()
+
+    const tile = this.map.getTileAtWorldXY(coords.x,coords.y);//this.player.x,this.player.y);
+
+
+    // console.log(tile.getCenterX);
+    // console.log(tile);
+    if(setXorY){
+      this.player.setX(tile.getCenterX());
+    }
+    else{
+      this.player.setY(tile.getCenterY())
+    }
+    // this.player.setX(tile.getCenterX()).setY(tile.getCenterY());
+  }
+
+  handleMovement(){
     const { left, right, up, down } = this.cursor;
 
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  
+    
     if (left.isDown || this.keyA.isDown) {
       this.player.setVelocityX(-this.playerSpeed);
       this.player.setVelocityY(0);
       this.player.setRotation(-Math.PI / 2);
-      this.player.setSize(61, 36)
+      this.player.setSize(61, 36);
       //make sure to adjust the size of the thing!
+      this.setPlayerToNearestCord(false );
     }
     else if (right.isDown || this.keyD.isDown) {
       this.player.setVelocityX(this.playerSpeed);
       this.player.setVelocityY(0);
       this.player.setRotation(Math.PI / 2);
-      this.player.setSize(61, 36)
+      this.player.setSize(61, 36);
+      this.setPlayerToNearestCord(false);
     }
     else if (down.isDown || this.keyS.isDown) {
       this.player.setVelocityX(0);
       this.player.setVelocityY(this.playerSpeed);
       this.player.setRotation(Math.PI);
       this.player.setSize(36, 61)
+      this.setPlayerToNearestCord(true);
     }
     else if (up.isDown || this.keyW.isDown) {
       this.player.setVelocityX(0);
       this.player.setVelocityY(-this.playerSpeed);
       this.player.setRotation(0);
       this.player.setSize(36, 61);
+      this.setPlayerToNearestCord(true);
     }
-
-  }
-
-  teleportPlayer(){
-    console.log("SUCCESS!");
   }
 }
 
-let catchPause = false;
+let isPaused = false;
 
 //FEATURE
 document.querySelector("button").addEventListener("click",(e)=>{
-  catchPause = !catchPause;
-  
-  if(catchPause){
-    game.pause();
-  }
-  else{
-    game.resume();
-  }
+  console.log("hi");
+  if(isPaused) game.resume();
+  else game.pause();
+
+  isPaused = !isPaused;
 });
 
 //we need at least a 5:4 ratio for the coord sys
---
+++ tiledAssets/backgroundTitles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="backgroundTitles" tilewidth="30" tileheight="24" tilecount="961" columns="31">
+ <image source="../../../../Cide/project-car-hunt/public/bg.png" width="938" height="750"/>
+</tileset>
