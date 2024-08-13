af419473807e6d18796800b5c794998b0f1edd18
Disguied-Coffee <danielidanque@gmail.com>
2024-08-13 08:49:58 -0500

    Potential multiatlas fix?

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,10 +3,12 @@ import CityScapeData from "./tiledAssets/mapTest3/map.json"
 import CityScape from "./terrain.png"
 
 import playerAtlasData from "./atlas/blah.json"
+import test from './atlas/blah.png'
 // import test from "./atlas"
 
 export const atlas = {
-    playerAtlas : playerAtlasData
+    playerAtlas : playerAtlasData,
+    image : test
 }
 
 export const moveMeLater = {
